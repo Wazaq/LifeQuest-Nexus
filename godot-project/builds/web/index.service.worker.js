2f883e5ac802b7fa754df4419287f909dca37f1d
Brent <Brent.Watkins32@gmail.com>
2025-07-06 18:50:47 -0500

    Forgot to export the package from Godot after fixing the buttons.

--- godot-project/builds/web/index.service.worker.js
+++ godot-project/builds/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1751809242|93415359448';
+const CACHE_VERSION = '1751845791|129965637566';
 /** @type {string} */
 const CACHE_PREFIX = 'LifeQuest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
