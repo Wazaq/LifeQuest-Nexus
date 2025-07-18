780d4b8fb20521b1b8777e77056acd0c503856b2
Brent <Brent.Watkins32@gmail.com>
2025-07-17 20:23:16 -0500

    Netlify: Cache testing

--- godot-project/builds/web/index.service.worker.js
+++ godot-project/builds/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1752790305|120801857928';
+const CACHE_VERSION = '1752801744|132241189956';
 /** @type {string} */
 const CACHE_PREFIX = 'LifeQuest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
