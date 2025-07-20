dfa98a2c3b0e92fee9bf2930733698f05cb7fb8e
Brent <Brent.Watkins32@gmail.com>
2025-07-20 12:41:57 -0500

    test - Cahcing 008 - Testing updates on webapps that remain open

--- godot-project/builds/web/index.service.worker.js
+++ godot-project/builds/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753032608|18750213643';
+const CACHE_VERSION = '1753033273|19415087956';
 /** @type {string} */
 const CACHE_PREFIX = 'LifeQuest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
