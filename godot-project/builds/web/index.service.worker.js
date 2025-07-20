5715dee486fc6ed8fea2111f009a42c6febea598
Brent <Brent.Watkins32@gmail.com>
2025-07-20 11:41:16 -0500

    Test -Cache Busting test 005

--- godot-project/builds/web/index.service.worker.js
+++ godot-project/builds/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753024945|11086786345';
+const CACHE_VERSION = '1753029644|15786186341';
 /** @type {string} */
 const CACHE_PREFIX = 'LifeQuest-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
